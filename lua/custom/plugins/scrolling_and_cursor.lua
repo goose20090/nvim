305fec9e002754f31afa84aab93b52178ed43575
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2025-07-20 13:01:43 +0100

    scrolling and cursor plugins file
    adds some scrolling and cursor animations and a scrollbar

---
+++ lua/custom/plugins/scrolling_and_cursor.lua
@@ -0,0 +1,31 @@
+return {
+  {
+    'karb94/neoscroll.nvim',
+    event = 'VeryLazy',
+    opts = {
+      hide_cursor = true,
+      duration_multiplier = 0.35,
+      mappings = {
+        -- keys to be mapped to their corresponding default scrolling animation
+        '<C-u>',
+        '<C-d>',
+        '<C-b>',
+        '<C-y>',
+        '<C-e>',
+        'zt',
+        'zz',
+        'zb',
+      },
+    },
+  },
+  {
+    'sphamba/smear-cursor.nvim',
+    event = 'VeryLazy',
+    opts = {
+      stiffness = 0.7,
+    },
+  },
+  {
+    'dstein64/nvim-scrollview',
+  },
+}
