e684b10bc3ddc656314bb2d59746c795b257f0e0
Angus Bower-Brown <angus.bower-brown@dentally.co>
2025-07-21 21:50:24 +0100

    add autotag
    closes html-esque tags

--- init.lua
+++ init.lua
@@ -454,3 +454,20 @@ end)
 vim.keymap.set('n', '<C-S-N>', function()
   harpoon:list():next()
 end)
+
+require('nvim-ts-autotag').setup {
+  opts = {
+    -- Defaults
+    enable_close = true, -- Auto close tags
+    enable_rename = true, -- Auto rename pairs of tags
+    enable_close_on_slash = false, -- Auto close on trailing </
+  },
+  -- Also override individual filetype configs, these take priority.
+  -- Empty by default, useful if one of the "opts" global settings
+  -- doesn't work well in a specific filetype
+  per_filetype = {
+    ['html'] = {
+      enable_close = false,
+    },
+  },
+}
