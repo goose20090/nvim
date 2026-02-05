9a56ffbda0c7cf37f53b83ec7f2810e49faee006
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2026-02-05 20:18:38 +0000

    comment out startup
    using snacks one for now

--- lua/custom/plugins/startup.lua
+++ lua/custom/plugins/startup.lua
@@ -1,12 +1,13 @@
-vim.g.startup_bookmarks = {
-  ['N'] = '~/.config/nvim/init.lua',
-  ['F'] = '~/.config/fish/config.fish',
-  ['G'] = '~/.config/ghostty/config',
-}
-return {
-  'startup-nvim/startup.nvim',
-  dependencies = { 'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope-file-browser.nvim' },
-  config = function()
-    require('startup').setup { theme = 'my_theme' }
-  end,
-}
+return {}
+-- vim.g.startup_bookmarks = {
+--   ['N'] = '~/.config/nvim/init.lua',
+--   ['F'] = '~/.config/fish/config.fish',
+--   ['G'] = '~/.config/ghostty/config',
+-- }
+-- {
+--   'startup-nvim/startup.nvim',
+--   dependencies = { 'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope-file-browser.nvim' },
+--   config = function()
+--     require('startup').setup { theme = 'my_theme' }
+--   end,
+-- }
