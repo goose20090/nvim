b9933b5a6ee31726fa4abca6b24f926a174d9d1b
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2026-02-05 10:44:44 +0000

    use github dark for colorscheme
    plays with snacks pickers better

--- lua/custom/plugins/colorschmes.lua
+++ lua/custom/plugins/colorschmes.lua
@@ -2,6 +2,11 @@ return {
   {
     'projekt0n/github-nvim-theme',
     name = 'github-theme',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme 'github_dark_high_contrast'
+    end,
   },
 
   {
