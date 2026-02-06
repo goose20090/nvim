73902d4855f551a09a2ea8075d57df35da0022a0
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2026-02-06 17:54:31 +0000

    get rid of duplicate autotag

--- lua/plugins/autotag.lua
+++ lua/plugins/autotag.lua
@@ -1,9 +1,12 @@
 return {
   'windwp/nvim-ts-autotag',
+  event = { 'InsertEnter' },
   opts = {
-    enable_close = true,
-    enable_rename = true,
-    enable_close_on_slash = false,
+    opts = {
+      enable_close = true,
+      enable_rename = true,
+      enable_close_on_slash = false,
+    },
     per_filetype = {
       ['html'] = {
         enable_close = false,
