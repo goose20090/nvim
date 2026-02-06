afaf897bb681c60c21f0c53a23b4e6e4f596e6f7
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2026-02-06 18:12:55 +0000

    put harpoon in its own file

--- init.lua
+++ init.lua
@@ -17,7 +17,7 @@ vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
--- Load vim bindings
+-- Load vim config and keybindings
 
 require 'options'
 require 'keymaps'
@@ -25,29 +25,6 @@ require 'autocmds'
 
 -- Load plugins with lazy
 
-require('lazy').setup {
-
-  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
-
-  { import = 'plugins' },
-}
-
-local harpoon = require 'harpoon'
-
-harpoon:setup()
-
-vim.keymap.set('n', '<leader>a', function() harpoon:list():add() end)
-vim.keymap.set('n', '<C-e>', function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-
-vim.keymap.set('n', '<C-1>', function() harpoon:list():select(1) end)
-vim.keymap.set('n', '<C-2>', function() harpoon:list():select(2) end)
-vim.keymap.set('n', '<C-3>', function() harpoon:list():select(3) end)
-vim.keymap.set('n', '<C-4>', function() harpoon:list():select(4) end)
-
--- Toggle previous & next buffers stored within Harpoon list
-vim.keymap.set('n', '<C-S-P>', function() harpoon:list():prev() end)
-vim.keymap.set('n', '<C-S-N>', function() harpoon:list():next() end)
-
-require('oil').setup()
+require('lazy').setup { import = 'plugins' }
 
 vim.lsp.enable 'ruby-lsp'
