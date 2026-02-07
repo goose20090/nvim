c8e81b837fba4e7bfa26cd754548476710b47351
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2026-02-07 14:00:34 +0000

    tweak harpoon to use opt instead of cntrl

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -9,10 +9,10 @@ return {
     vim.keymap.set('n', '<leader>a', function() harpoon:list():add() end, { desc = 'Harpoon: Add file' })
     vim.keymap.set('n', '<C-e>', function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = 'Harpoon: Toggle menu' })
 
-    vim.keymap.set('n', '<C-1>', function() harpoon:list():select(1) end, { desc = 'Harpoon: File 1' })
-    vim.keymap.set('n', '<C-2>', function() harpoon:list():select(2) end, { desc = 'Harpoon: File 2' })
-    vim.keymap.set('n', '<C-3>', function() harpoon:list():select(3) end, { desc = 'Harpoon: File 3' })
-    vim.keymap.set('n', '<C-4>', function() harpoon:list():select(4) end, { desc = 'Harpoon: File 4' })
+    vim.keymap.set('n', '<M-h>', function() harpoon:list():select(1) end, { desc = 'Harpoon: File 1' })
+    vim.keymap.set('n', '<M-j>', function() harpoon:list():select(2) end, { desc = 'Harpoon: File 2' })
+    vim.keymap.set('n', '<M-k>', function() harpoon:list():select(3) end, { desc = 'Harpoon: File 3' })
+    vim.keymap.set('n', '<M-l>', function() harpoon:list():select(4) end, { desc = 'Harpoon: File 4' })
 
     vim.keymap.set('n', '<C-S-P>', function() harpoon:list():prev() end, { desc = 'Harpoon: Previous' })
     vim.keymap.set('n', '<C-S-N>', function() harpoon:list():next() end, { desc = 'Harpoon: Next' })
