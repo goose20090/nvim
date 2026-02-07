f0c06eb17316c4bb8ed622e70e53eacf3268f5d3
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2026-02-07 12:30:57 +0000

    copilot lua over copilot vim
    not convinced, but am trying it out

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,3 +1,29 @@
 return {
-  { 'github/copilot.vim' },
+  'zbirenbaum/copilot.lua',
+  dependencies = {
+    'copilotlsp-nvim/copilot-lsp', -- optional for NES functionality
+  },
+  cmd = 'Copilot',
+  event = 'InsertEnter',
+  keys = {
+    {
+      '<leader>tc',
+      function()
+        require('copilot.suggestion').toggle_auto_trigger()
+        local enabled = vim.b.copilot_suggestion_auto_trigger
+        vim.notify(enabled and 'Copilot enabled' or 'Copilot disabled')
+      end,
+      desc = '[T]oggle [C]opilot',
+    },
+  },
+  config = function()
+    require('copilot').setup {
+      suggestion = {
+        auto_trigger = true,
+        keymap = {
+          accept = '<Tab>',
+        },
+      },
+    }
+  end,
 }
