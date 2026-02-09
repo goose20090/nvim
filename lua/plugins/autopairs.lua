91e8fd48410055cbfd6ec2dee6375b6de3e1c043
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2026-02-09 13:58:30 +0000

    add autopairs along with erb specific helper for <%%> and<%=%>

---
+++ lua/plugins/autopairs.lua
@@ -0,0 +1,15 @@
+return {
+  'windwp/nvim-autopairs',
+  event = 'InsertEnter',
+  config = function()
+    local npairs = require 'nvim-autopairs'
+    local Rule = require 'nvim-autopairs.rule'
+    local cond = require 'nvim-autopairs.conds'
+
+    npairs.setup {}
+
+    npairs.add_rules {
+      Rule('<%', '%>', 'eruby'),
+    }
+  end,
+}
